1ab9bd66f4857e7885fab7685f0f3c940e55cfc2
mousaabdulnour <109285396+mousaabdulnour@users.noreply.github.com>
2023-01-21 16:36:10 -0300

    experiecia y educacion

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.comp
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeMiComponent
+    AcercaDeMiComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
